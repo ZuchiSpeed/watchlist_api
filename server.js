0d8ace0fe6eea258df030831e44a96454ad74b22
ZuchiSpeed <zuchi737@gmail.com>
2026-04-18 12:31:13 +0200

    watchlist routes & controllers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import movieRoutes from "./routes/movieRoutes.js";
 import { config } from "dotenv";
 import { connectDB, disconnectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import watchlistRoutes from './routes/watchlistRoutes.js'
 
 config();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
+app.use('/watchlist', watchlistRoutes)
 
 const startServer = async () => {
   try {
